f0edf6a5c023d0f67f4f4f9b976dade56cc9d4b0
Cabrel10 <cabrelkaka@gmail.com>
2025-06-10 22:41:06 +0200

    composant

--- components/icons/QrCodeIcon.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-
-export const QrCodeIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 4.5A.75.75 0 014.5 3.75h4.5a.75.75 0 01.75.75v4.5a.75.75 0 01-.75.75h-4.5a.75.75 0 01-.75-.75v-4.5zM3.75 15A.75.75 0 014.5 14.25h4.5a.75.75 0 01.75.75v4.5a.75.75 0 01-.75.75h-4.5a.75.75 0 01-.75-.75v-4.5zM15 3.75A.75.75 0 0014.25 3h-4.5a.75.75 0 00-.75.75v4.5a.75.75 0 00.75.75h4.5a.75.75 0 00.75-.75v-4.5z" />
-        <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 15a.75.75 0 01.75.75v4.5a.75.75 0 01-.75.75h-4.5a.75.75 0 01-.75-.75V15a.75.75 0 01.75-.75h4.5zM8.25 11.25h1.5V12h-1.5v-1.5zm1.5 1.5v1.5H12v-1.5h-2.25zM15 11.25H9.75v1.5H15V11.25zM11.25 8.25v1.5H12V8.25h-2.25zM15 8.25h-1.5v1.5H15V8.25zM8.25 3.75v1.5H9V3.75H8.25z" />
-    </svg>
-);
